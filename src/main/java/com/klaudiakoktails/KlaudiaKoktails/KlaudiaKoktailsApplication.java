e241c13c0604f1427bb130fddf92402d454ca4cc
Klaudia <klaudia.loviskova@gmail.com>
2020-04-12 14:41:56 +0200

    Spring inicializer

---
+++ src/main/java/com/klaudiakoktails/KlaudiaKoktails/KlaudiaKoktailsApplication.java
@@ -0,0 +1,13 @@
+package com.klaudiakoktails.KlaudiaKoktails;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KlaudiaKoktailsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KlaudiaKoktailsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/klaudiakoktails/KlaudiaKoktails/KlaudiaKoktailsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.klaudiakoktails.KlaudiaKoktails;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KlaudiaKoktailsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
