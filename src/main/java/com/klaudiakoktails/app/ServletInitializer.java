b13bcc5de897b366bc9f5d743a584a72808330f3
Klaudia <klaudia.loviskova@gmail.com>
2020-04-14 21:57:58 +0200

    app properties

--- src/main/java/com/klaudiakoktails/app/ServletInitializer.java
+++ src/main/java/com/klaudiakoktails/app/ServletInitializer.java
@@ -1,4 +1,4 @@
-package com.klaudiakoktails.KlaudiaKoktails;
+package com.klaudiakoktails.app;
 
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
